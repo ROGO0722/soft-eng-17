c6956a86234d3c3acf1673c28a947b1bf3f9c9f6
ROGO0722 <151502519+ROGO0722@users.noreply.github.com>
2023-11-20 23:56:00 -0500

    Add files via upload

---
+++ jrails/src/main/java/jrails/JRouter.java
@@ -0,0 +1,57 @@
+package jrails;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import javax.swing.text.html.HTML;
+
+import java.lang.reflect.*;
+
+
+public class JRouter {
+    private Map<String, Map <String, String>> routes;
+    public JRouter() {
+        this.routes = new HashMap<>();
+    }
+
+    public void addRoute(String verb, String path, Class clazz, String method) {
+        String controllerMethod = clazz.getName() + "#" + method;
+        Map<String, String> param = routes.computeIfAbsent(verb, k -> new HashMap<>());
+        param.put(path, controllerMethod);
+    }
+
+    // Returns "clazz#method" corresponding to verb+URN
+    // Null if no such route
+    public String getRoute(String verb, String path) {
+        Map<String, String> param = routes.get(verb);
+        if (param != null) {
+            return param.get(path);
+        }
+        return null;
+    }
+    
+    // Call the appropriate controller method and
+    // return the result
+    public Html route(String verb, String path, Map<String, String> params) {
+        String controllerMethod = getRoute(verb, path);
+        if(controllerMethod != null) {
+            String[] division = controllerMethod.split("#");
+            String className = division[0];
+            String methodName = division[1];
+            try {
+                Class<?> c = Class.forName(className);
+                Method method = c.getMethod(methodName, Map.class);
+                Object result = method.invoke(null, params);
+                if(!(result instanceof Html)) {
+                    throw new UnsupportedOperationException();
+                }
+                return (Html) result;
+            } catch (Exception e) {
+                throw new UnsupportedOperationException();
+            }
+        }else {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
